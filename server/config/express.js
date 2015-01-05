881e8529e8180604b1b54522fc591aae14e1fca0
Konstanting Gritsenko <kgritsenko@grossum.com>
2015-01-05 20:45:58 +0200

    base nodejs skeleton added

---
+++ server/config/express.js
@@ -0,0 +1,40 @@
+/**
+ * Express configuration
+ */
+
+'use strict';
+
+var express = require('express');
+var favicon = require('serve-favicon');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var errorHandler = require('errorhandler');
+var path = require('path');
+var config = require('./environment');
+
+module.exports = function(app) {
+  var env = app.get('env');
+
+  app.set('views', config.root + '/server/views');
+  app.set('view engine', 'html');
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.use(cookieParser());
+
+  if ('production' === env) {
+    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    app.use(express.static(path.join(config.root, '/build')));
+    app.set('appPath', config.root + '/production');
+  }
+
+  if ('development' === env || 'test' === env) {
+    app.use(express.static(path.join(config.root, '.tmp')));
+    app.use(express.static(path.join(config.root, '/mobile')));
+    app.set('appPath', config.root + '/development');
+    app.use(errorHandler()); // Error handler - has to be last
+  }
+};
